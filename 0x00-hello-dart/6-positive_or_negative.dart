6fca927278db0bbd60d5c23679fafed4551c842d
ilyes01 <ilyesghrab7@gmail.com>
2022-07-27 19:13:27 +0200

    if else

---
+++ 0x00-hello-dart/6-positive_or_negative.dart
@@ -0,0 +1,11 @@
+main void ()
+var number = int.parse(args[0]);
+ if (number == 0) {   
+   print("$number is zero");
+ } else if (number > 0) {
+   print("$number is positive");
+ } else {
+   print("$number is negative");
+ }
+}
+
