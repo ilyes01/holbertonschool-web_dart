822b871d3410357cf95d45579438ddff6eb36c6d
ilyes01 <ilyesghrab7@gmail.com>
2022-08-02 16:48:58 +0200

    temp

---
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -0,0 +1,8 @@
+List<double> convertToF(List<double> temperaturesInC){
+List<double> res = [];
+for (int i = 0; i < temperaturesInC.length; i++){
+    res.add((temperaturesInC(i) * (9 / 5)) + 32);
+}
+return res;
+}
+
