71885305e0fcaf2e09be4bb1be8cacfbf7ea3be3
ilyes01 <ilyesghrab7@gmail.com>
2022-08-02 16:10:33 +0200

    fixx

--- 0x01-dart-function/2-sumfunc.dart
+++ 0x01-dart-function/2-sumfunc.dart
@@ -1,8 +1,8 @@
 int add(int a, int b){
-return ($a + $b); 
+return a + b; 
 }
 int sub(int a, int b){
-return ($a - $b);
+return a - b;
 }
 String showFunc(int a, int b){
 return("Add $a + $b = add(a, b)\nSub $a - $b = sub(a, b));
