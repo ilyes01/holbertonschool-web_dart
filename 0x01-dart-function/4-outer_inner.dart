cd0029d64319ff59eb53d9d834e0c61fcff1f7be
ilyes01 <ilyesghrab7@gmail.com>
2022-08-02 16:31:06 +0200

    inner

---
+++ 0x01-dart-function/4-outer_inner.dart
@@ -0,0 +1,8 @@
+void outer(String name, String id){
+ String inner() {
+    var list = name.split(" ");
+    var res = splitter[1].substring(0, 1) + "." + splitter[0];
+    return "Hello Agent $res your id is $id";
+  }
+  print(inner());
+}
