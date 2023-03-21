ff484b28a7a4fa3119a66f657f1176c28a39d0ae
ilyes01 <ilyesghrab7@gmail.com>
2023-03-21 08:41:13 -0700

    palindrome

--- 0x01-dart-function/9-palindrome.dart
+++ 0x01-dart-function/9-palindrome.dart
@@ -1,13 +1,14 @@
-bool isPalindrome(String s) { 
-String inRev = ""; 
-if (inRev == s)
-    return true;
- if (s.length < 3)
-     return false;
+bool isPalindrome(String s) {
+  if (s.length < 3) {
+    return false;
+  }
 
-   for (var x = s.length - 1; x >= 0; x--) {
-     inRev += s[x];
-   }
- else
-       return false;
+  for (int i = 0; i < s.length ~/ 2; i++) {
+    if (s[i] != s[s.length - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
 }
+
