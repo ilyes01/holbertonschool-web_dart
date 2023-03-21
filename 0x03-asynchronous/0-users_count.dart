4b391c5cafc206d100f9ffdb6ed3347402740b47
ilyes01 <ilyesghrab7@gmail.com>
2023-03-21 05:58:39 -0700

    update

--- 0x03-asynchronous/0-users_count.dart
+++ 0x03-asynchronous/0-users_count.dart
@@ -1,15 +1,7 @@
 import '0-util.dart';
 
-void main() async {
-  await usersCount();
-}
-
 Future<void> usersCount() async {
-  try {
-    var count = await fetchUsersCount();
-    print('Number of users: $count');
-  } catch (e) {
-    print('Error fetching user count: $e');
-  }
+  int count = await fetchUsersCount();
+  print(count);
 }
 
