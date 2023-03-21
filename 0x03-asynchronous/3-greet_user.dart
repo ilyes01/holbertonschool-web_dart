75ea3ca13dd5861fe769dc08eda0eadacb23d488
ilyes01 <ilyesghrab7@gmail.com>
2023-03-21 06:32:46 -0700

    update

--- 0x03-asynchronous/3-greet_user.dart
+++ 0x03-asynchronous/3-greet_user.dart
@@ -3,23 +3,8 @@ import '3-util.dart';
 Future<String> greetUser() async {
   try {
     final userData = await fetchUserData();
-    final username = userData['username'];
+    final username = json.decode(userData)['username'];
     return 'Hello $username';
-  } catch (e) {
-    return 'error caught: $e';
-  }
-}
-
-Future<String> loginUser() async {
-  try {
-    final isUser = await checkCredentials();
-    print('There is a user: $isUser');
-    if (isUser) {
-      final greeting = await greetUser();
-      return greeting;
-    } else {
-      return 'Wrong credentials';
-    }
   } catch (e) {
     return 'error caught: $e';
   }
