7b34ef2261a9be8f0e91b7f35448e4f2310b5528
ilyes01 <ilyesghrab7@gmail.com>
2023-03-22 02:55:51 -0700

    prv

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,31 @@
+class Password {
+  String _password;
+
+  Password({required String password}) : _password = password;
+
+  bool isValid() {
+    if (_password.length < 8 || _password.length > 16) {
+      return false;
+    }
+
+    if (!_password.contains(RegExp(r'[A-Z]'))) {
+      return false;
+    }
+
+    if (!_password.contains(RegExp(r'[a-z]'))) {
+      return false;
+    }
+
+    if (!_password.contains(RegExp(r'[0-9]'))) {
+      return false;
+    }
+
+    return true;
+  }
+
+  @override
+  String toString() {
+    return 'Your Password is: $_password';
+  }
+}
+
