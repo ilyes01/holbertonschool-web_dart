f6bcf4dae61761709682b67813af2078aa00e648
ilyes01 <ilyesghrab7@gmail.com>
2023-03-22 04:28:42 -0700

    json user

---
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -0,0 +1,32 @@
+class User {
+  final int id;
+  final String name;
+  final int age;
+  final double height;
+
+  User({required this.id, required this.name, required this.age, required this.height});
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+      'height': height,
+    };
+  }
+
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+      id: userJson['id'],
+      name: userJson['name'],
+      age: userJson['age'],
+      height: userJson['height'],
+    );
+  }
+
+  @override
+  String toString() {
+    return 'User(id : $id ,name: $name, age: $age, height: $height)';
+  }
+}
+
