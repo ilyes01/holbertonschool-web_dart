84a3820e2b70aaab867824b06032a2615dab94f9
ilyes01 <ilyesghrab7@gmail.com>
2023-05-18 13:32:17 +0100

    updatee

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:convert';
-import '4-util.dart';
 
 Future<double> calculateTotal() async {
   try {
@@ -20,6 +19,33 @@ Future<double> calculateTotal() async {
   }
 }
 
+Future<String> fetchUserOrders(String id) async {
+  var orders = {
+    "7ee9a243-01ca-47c9-aa14-0149789764c3": ["pizza", "orange"]
+  };
+  try {
+    return Future.delayed(
+        const Duration(seconds: 2), () => json.encode(orders[id]));
+  } catch (err) {
+    return "error caught: $err";
+  }
+}
+
+Future<String> fetchUserData() => Future.delayed(
+      const Duration(seconds: 2),
+      () =>
+          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
+    );
+
+Future<double> fetchProductPrice(String product) async {
+  var products = {"pizza": 20.30, "orange": 10, "water": 5, "soda": 8.5};
+  try {
+    return Future.delayed(const Duration(seconds: 2), () => products[product]);
+  } catch (err) {
+    return -1;
+  }
+}
+
 Map<String, dynamic> parseUserData(String userData) {
   final userMap = json.decode(userData);
   return userMap;
@@ -30,8 +56,11 @@ List<String> parseUserOrders(String userOrders) {
   return List<String>.from(ordersList);
 }
 
-double parseProductPrice(String productPrice) {
-  final price = double.parse(productPrice);
-  return price;
+double parseProductPrice(double productPrice) {
+  return productPrice;
+}
+
+void main() async {
+  print(await calculateTotal());
 }
 
