5a17a155564188107448d2820312f7053d189bfb
ilyes01 <ilyesghrab7@gmail.com>
2023-05-18 13:23:21 +0100

    back

--- 0x03-asynchronous/3-greet_user.dart
+++ 0x03-asynchronous/3-greet_user.dart
@@ -1,12 +1,34 @@
+import 'dart:async';
 import '3-util.dart';
 
 Future<String> greetUser() async {
   try {
     final userData = await fetchUserData();
-    final username = json.decode(userData)['username'];
-    return 'Hello $username';
-  } catch (e) {
-    return 'error caught: $e';
+    final user = parseUserData(userData);
+    return 'Hello ${user["username"]}';
+  } catch (error) {
+    return 'error caught: $error';
   }
 }
+
+Future<String> loginUser() async {
+  try {
+    final isUser = await checkCredentials();
+    print('There is a user: $isUser');
+    if (isUser) {
+      return await greetUser();
+    } else {
+      return 'Wrong credentials';
+    }
+  } catch (error) {
+    return 'error caught: $error';
+  }
+}
+
+Map<String, dynamic> parseUserData(String userData) {
+  // Parse the user data JSON and return it as a Map
+  final userMap = jsonDecode(userData);
+  return userMap;
+}
+
 
