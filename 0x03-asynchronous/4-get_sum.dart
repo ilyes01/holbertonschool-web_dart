08c6ebc00b10e194f3234bc20ef13e1cb9f95d58
ilyes01 <ilyesghrab7@gmail.com>
2023-05-18 13:44:09 +0100

    oooo

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -4,13 +4,13 @@ import 'dart:convert';
 Future<double> calculateTotal() async {
   try {
     final userData = await fetchUserData();
-    final user = parseUserData(userData);
-    final userOrders = await fetchUserOrders(user["id"]);
-    final orders = parseUserOrders(userOrders);
+    final user = json.decode(userData);
+    final userOrders = await fetchUserOrders(user['id']);
+    final orders = json.decode(userOrders);
     double totalPrice = 0;
     for (var order in orders) {
       final productPrice = await fetchProductPrice(order);
-      final price = parseProductPrice(productPrice);
+      final price = double.parse(productPrice);
       totalPrice += price;
     }
     return totalPrice;
@@ -37,30 +37,12 @@ Future<String> fetchUserData() => Future.delayed(
           '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
     );
 
-Future<double> fetchProductPrice(String product) async {
-  var products = {"pizza": 20.30, "orange": 10, "water": 5, "soda": 8.5};
+Future<String> fetchProductPrice(String product) async {
+  var products = {"pizza": "20.30", "orange": "10", "water": "5", "soda": "8.5"};
   try {
     return Future.delayed(const Duration(seconds: 2), () => products[product]);
   } catch (err) {
-    return -1;
+    return "error caught: $err";
   }
-}
-
-Map<String, dynamic> parseUserData(String userData) {
-  final userMap = json.decode(userData);
-  return userMap;
-}
-
-List<String> parseUserOrders(String userOrders) {
-  final ordersList = json.decode(userOrders);
-  return List<String>.from(ordersList);
-}
-
-double parseProductPrice(double productPrice) {
-  return productPrice;
-}
-
-void main() async {
-  print(await calculateTotal());
 }
 
