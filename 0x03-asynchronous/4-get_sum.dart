2ca510dfcda30f6d8a96045ce4cf059322766507
ilyes01 <ilyesghrab7@gmail.com>
2023-05-18 13:27:50 +0100

    update

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,12 +1,37 @@
+import 'dart:async';
 import 'dart:convert';
+import '4-util.dart';
 
 Future<double> calculateTotal() async {
-var products = {"pizza": 20.30, "orange": 10, "water": 5, "soda": 8.5};
   try {
-    return Future.delayed(
-        const Duration(seconds: 2), () => products[product]);
-  } catch (err) {
+    final userData = await fetchUserData();
+    final user = parseUserData(userData);
+    final userOrders = await fetchUserOrders(user["id"]);
+    final orders = parseUserOrders(userOrders);
+    double totalPrice = 0;
+    for (var order in orders) {
+      final productPrice = await fetchProductPrice(order);
+      final price = parseProductPrice(productPrice);
+      totalPrice += price;
+    }
+    return totalPrice;
+  } catch (error) {
     return -1;
   }
+}
+
+Map<String, dynamic> parseUserData(String userData) {
+  final userMap = json.decode(userData);
+  return userMap;
+}
+
+List<String> parseUserOrders(String userOrders) {
+  final ordersList = json.decode(userOrders);
+  return List<String>.from(ordersList);
+}
+
+double parseProductPrice(String productPrice) {
+  final price = double.parse(productPrice);
+  return price;
 }
 
